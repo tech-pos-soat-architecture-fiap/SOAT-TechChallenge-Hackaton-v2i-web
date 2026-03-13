1d9617e036272d4b5afa34f59ada41308a238e06
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2026-03-12 21:01:13 -0300

    Apply suggestion from @lucas-peixoto

--- src/main/java/br/com/fiap/v2i/user/User.java
+++ src/main/java/br/com/fiap/v2i/user/User.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
-//TODO talvez precise adicionar mais atributos aqui
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
