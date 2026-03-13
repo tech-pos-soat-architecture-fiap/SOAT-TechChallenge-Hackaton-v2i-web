bef6fb7458fe72ffea8e2f5a27dc018c7091fcf8
Edir Lucas Icety <icetylucas@gmail.com>
2026-03-13 16:42:07 -0300

    adicionando swagger

---
+++ src/main/java/br/com/fiap/v2i/infrastructure/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package br.com.fiap.v2i.infrastructure;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Notification Uploaded Video API")
+                        .version("1.0")
+                        .description("Documentation of the API using Swagger OpenAPI"));
+    }
+}
